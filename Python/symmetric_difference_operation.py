0077fdb7ea091cf94b20241982cabd4aee6e000f
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-26 23:47:55 +0200

    Add solution for task: Set .symmetric_difference() Operation

---
+++ Python/symmetric_difference_operation.py
@@ -0,0 +1,16 @@
+import sys
+from io import StringIO
+
+test1 = '''9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8'''
+
+sys.stdin = StringIO(test1)
+
+_ = int(input())
+a = set(map(int, input().split()))
+_ = int(input())
+b = set(map(int, input().split()))
+
+print(len(a.symmetric_difference(b)))
