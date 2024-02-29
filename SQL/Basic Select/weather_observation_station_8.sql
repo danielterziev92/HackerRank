873823dd050467398f23ccb12cd8e2ae612ea210
danielterziev92 <daniel.st.terziev@gmail.com>
2024-02-29 13:51:41 +0200

    Add solution for task: Weather observation station 8

---
+++ SQL/Basic Select/weather_observation_station_8.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS STATION;
+CREATE TABLE STATION
+(
+    id    int,
+    CITY  varchar(21),
+    STATE varchar(2),
+    LAT_N double,
+    LONG_W double
+);
+
+INSERT INTO STATION
+VALUES (1, 'DEF', '', 1, 1),
+       (2, 'ABC', '', 1, 1),
+       (3, 'PQRS', '', 1, 1),
+       (4, 'WXY', '', 1, 1);
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(CITY) REGEXP '^[aeiouAEIOU]'
+  AND LOWER(CITY) REGEXP '[aeiouAEIOU]$';
