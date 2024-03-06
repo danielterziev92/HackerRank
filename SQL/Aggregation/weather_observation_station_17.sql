79b3bbe5f75a220a6264d5da43b0d1906a793c93
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-06 17:03:34 +0200

    Add solution for task: Weather observation station 17

---
+++ SQL/Aggregation/weather_observation_station_17.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS STATION;
+CREATE TABLE STATION
+(
+    ID     int,
+    CITY   varchar(21),
+    STATE  varchar(21),
+    LAT_N  int,
+    LONG_W int
+);
+
+SELECT ROUND(LONG_W, 4) AS western_longitude
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N)
+               FROM STATION
+               WHERE LAT_N > 38.7780);
