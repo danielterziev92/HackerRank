0db827c56064e22448ae0c2e1ac6b06718929ad5
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-07 13:35:55 +0200

    Add solution for task: African cities

---
+++ SQL/Basic Join/african_cities.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS COUNTRY;
+DROP TABLE IF EXISTS CITY;
+
+CREATE TABLE COUNTRY
+(
+    CODE           varchar(3),
+    NAME           varchar(44),
+    CONTINENT      varchar(13),
+    REGION         varchar(25),
+    SURFACEAREA    int,
+    INDEPYEAR      varchar(5),
+    POPULATION     int,
+    LIFEEXPECTANCY varchar(4),
+    GNP            int,
+    GNPOLD         varchar(9),
+    LOCALNAME      varchar(44),
+    GOVERMENTFORM  varchar(44),
+    HEADOFSTATE    varchar(32),
+    CAPITAL        varchar(4),
+    CODE2          varchar(2)
+);
+
+CREATE TABLE CITY
+(
+    ID          int,
+    NAME        varchar(17),
+    COUNTRYCODE varchar(3),
+    DISTRICT    varchar(20),
+    POPULATION  int
+);
+
+SELECT CITY.Name
+FROM CITY
+         JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+WHERE COUNTRY.CONTINENT = 'Africa';
