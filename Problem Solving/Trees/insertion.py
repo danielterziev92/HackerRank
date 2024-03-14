31ffb887efbb4938c2c5dacf854c8726ceb8c044
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-14 14:01:53 +0200

    Add solution for task: Insertion

---
+++ Problem Solving/Trees/insertion.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+def preOrder(root):
+    if root == None:
+        return
+    print(root.info, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    # Node is defined as
+    # self.left (the left child of the node)
+    # self.right (the right child of the node)
+    # self.info (the value of the node)
+
+    def insert(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self._insert(self.root, val)
+
+    def _insert(self, current, val):
+        if val < current.info:
+            if current.left is None:
+                current.left = Node(val)
+            else:
+                self._insert(current.left, val)
+        elif val > current.info:
+            if current.right is None:
+                current.right = Node(val)
+            else:
+                self._insert(current.right, val)
+
+
+# Enter you code here.
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.insert(arr[i])
+
+preOrder(tree.root)
